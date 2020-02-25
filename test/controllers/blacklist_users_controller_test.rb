72f31006126d534d3d74f9a12084053bb471fca8
Sophie Archer <hello@sophiearcher.co.uk>
2020-02-25 20:48:13 +0000

    Delete blacklist_users_controller_test.rb

--- test/controllers/blacklist_users_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class BlacklistUsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get blacklist_users_create_url
-    assert_response :success
-  end
-
-end
