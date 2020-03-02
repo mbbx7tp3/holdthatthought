116357c49df30fd459059bf03b75d9b17053d7b4
Tom Poulter <tom.poulter.1@outlook.com>
2020-03-02 20:43:06 +0000

    schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_02_29_161505) do
-
+ActiveRecord::Schema.define(version: 2020_02_29_160323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,12 +79,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_161505) do
     t.index ["category_id"], name: "index_flashcards_on_category_id"
   end
 
-  create_table "plans", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "price_cents", default: 0, null: false
-  end
+
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
