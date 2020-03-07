528e424e938312b5ce1be9e2e4d08a785ab6bda9
Sophie Archer <hello@sophiearcher.co.uk>
2020-03-07 11:21:07 +0000

    bugs fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 puts "Cleaning Database"
 
+BlacklistUser.destroy_all
 Plan.destroy_all
 User.destroy_all
 Flashcard.destroy_all
@@ -61,8 +62,8 @@ puts "Created 4 blacklist websites..."
 
 # Assign blacklist websites to users
 
-# BlacklistUser.create(user: User.find(1), blacklist: Blacklist.find(1))
-# BlacklistUser.create(user: User.find(2), blacklist: Blacklist.find(2))
+# BlacklistUser.create(user: User.first, blacklist: Blacklist.first)
+# BlacklistUser.create(user: User.second, blacklist: Blacklist.second)
 # BlacklistUser.create(user: User.find(3), blacklist: Blacklist.find(3))
 # BlacklistUser.create(user: User.find(4), blacklist: Blacklist.find(4))
 
@@ -73,19 +74,19 @@ puts "Created 4 blacklist websites..."
 Flashcard.create(
   question: "How do you install gems you added to your Gemfile?",
   answer: "Kill any web server running (Ctrl + C). Run bundle install. Restart the Rails server with rails s",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
   question: "How do you build a link to the homepage of a Rails app?",
   answer: "rails new MY_APP",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
   question: "What is the default local port used by rails server?",
   answer: "3000, you can consult your app at localhost:3000",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
@@ -93,55 +94,55 @@ Flashcard.create(
   answer: "1 Kill any web server running (Ctrl + C)
           2 Run bundle install
           3 Restart the Rails server with rails s",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
   question: "How do you launch a local server with Rails?",
   answer: "rails server",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
   question: "What is an action in Rails?",
   answer: "An action is a controller’s method targeted by a route.",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
   question: "What is Rails?",
   answer: "Rails is a fullstack framework designed for web-app development based on Ruby & MVC-structured.",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
   question: "What is the command allowing you to list your app’s routes with Rails?",
   answer: "rails routes",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
   question: "How do you build the path to a page of a Rails app?",
   answer: "contact_path",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
   question: "Where can you find information and rating about a gem?",
   answer: "You can go to its GitHub repo and check stars, forks, contributors and last commit date. You can also go to rubygems.org to check stats.",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
   question: "Why are model validations so important?",
   answer: "Validations protect your database from incomplete or invalid records and gives error messages to display back to the user.",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
   question: "How does the development of any new rails app start?",
   answer: "<%= link_to 'Home', root_path %>",
-  category: Category.find(1)
+  category: Category.first
 )
 
 
@@ -153,7 +154,7 @@ Prefix is the rails path prefix of the associated route
 Verb is the HTTP verb of the route
 URI Pattern is the path of the route
 Controller#Action are targeted controller and method",
-  category: Category.find(1)
+  category: Category.first
 )
 
 Flashcard.create(
@@ -164,7 +165,7 @@ const name = ""Boris Paillard"";
 const firstName = ""Boris"";
 
 NB: That’s the same convention for functions.",
-  category: Category.find(2)
+  category: Category.second
 )
 
 Flashcard.create(
@@ -183,38 +184,38 @@ Reminder: the equivalent in ruby is:
 beatles.each do |beatle|
   puts beatle
 end",
-  category: Category.find(2)
+  category: Category.second
 )
 
 Flashcard.create(
   question: "In the HTML skeleton, which markup contains metas and which one contains page content?",
   answer: "<head> markup contains metas which are read by the browser. <body> markup contains content which is displayed to the client by the browser.",
-  category: Category.find(3)
+  category: Category.third
 )
 
 Flashcard.create(
   question: "How do you define a paragraph?",
   answer: "<p>A paragraph</p>",
-  category: Category.find(3)
+  category: Category.third
 )
 
 Flashcard.create(
   question: "How do you define the most important heading followed by the least important heading?",
   answer: "<h1>Title 1</h1>
 <h6>Title 6</h6>",
-  category: Category.find(3)
+  category: Category.third
 )
 
 Flashcard.create(
   question: "What are the four types of CSS units?",
   answer: "Units can be absolute in px, relative to the parent element in %, relative to the viewport (window) height vh or width vw, or relative to the font size of the element in em.",
-  category: Category.find(3)
+  category: Category.third
 )
 
 Flashcard.create(
   question: "What are the four types of CSS units?",
   answer: "Units can be absolute in px, relative to the parent element in %, relative to the viewport (window) height vh or width vw, or relative to the font size of the element in em.",
-  category: Category.find(3)
+  category: Category.third
 )
 
 puts "Created 5 flashcards..."
