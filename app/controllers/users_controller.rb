09dcfbbf11d0866e7387a1e3893b4be40a1eb1e1
Tom Poulter <tom.poulter.1@outlook.com>
2020-03-07 15:26:46 +0000

    redirect done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,11 @@ class UsersController < ApplicationController
 
     @blacklist_ids.each do |blacklist_id|
       BlacklistUser.create(user: @user, blacklist: Blacklist.find(blacklist_id.to_i))
+
+    if @user.save
+      redirect_to dashboard_path
     end
+  end
 
     # @user.update(Hash[@blacklist_ids])
     # @user.update(blacklist_ids: @blacklist_ids)
@@ -21,6 +25,6 @@ class UsersController < ApplicationController
 
     # raise
 
-  end
+end
 
 end
