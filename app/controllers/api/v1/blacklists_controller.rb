1251d933ec32ef09c107bbd136e6c50ef4c85a75
Jamie Reynolds <jamie.s.a.reynolds@gmail.com>
2020-03-10 14:25:00 +0000

    Second fix for blacklists bug

--- app/controllers/api/v1/blacklists_controller.rb
+++ app/controllers/api/v1/blacklists_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::BlacklistsController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User
 
   def index
-    # @blacklists = policy_scope(Blacklist)
+    @blacklists = policy_scope(Blacklist)
+    authorize @blacklists
 
     user_email = request.env["HTTP_X_USER_EMAIL"]
     user = User.find_by(email: user_email)
