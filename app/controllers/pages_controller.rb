f1c650ff9f4da1f9084aef1a181e9ff1f567ea33
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-03-10 20:51:15 +0000

    Fixed navbar to flashcards link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,7 @@ class PagesController < ApplicationController
 
   def dashboard
     @user = current_user
-    @flashcard_users = FlashcardUser.last
+    @flashcard = FlashcardUser.where(user_id: @user.id).where(completed: false).first.flashcard
   end
 
   def payment
