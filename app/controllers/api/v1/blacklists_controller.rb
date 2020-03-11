d563d0eba59cd94ba852384c9138193e5f27b5e0
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-03-11 19:49:52 +0000

    Blacklist#create NEW API working

--- app/controllers/api/v1/blacklists_controller.rb
+++ app/controllers/api/v1/blacklists_controller.rb
@@ -13,16 +13,32 @@ class Api::V1::BlacklistsController < Api::V1::BaseController
 
   def create
     user_email = request.env["HTTP_X_USER_EMAIL"]
-    user = User.find_by(email: user_email)
+    user_token = request.env["HTTP_X_USER_TOKEN"]
 
-    blacklist_id = request.env["HTTP_X_BLACKLIST_ID"]
-    blacklist = Blacklist.find_by(id: blacklist_id)
+    user = User.find_by(email: user_email, authentication_token: user_token)
 
-    blacklist_user = BlacklistUser.create(user: user, blacklist: blacklist)
+    if user
 
-    blacklist_user_all = BlacklistUser.where(user: user)
-    @updated_blacklists = blacklist_user_all.map do |blacklist_user|
-      Blacklist.find(blacklist_user.blacklist_id)
+      # Check within extension background.js whether website_name already exists
+
+      # 1. Create Blacklist
+      # If extension doesn't provide ID => blacklist doesn't yest exist and needs to be created new
+      # blacklist_id should be nill
+      blacklist_id = request.env["HTTP_X_BLACKLIST_ID"]
+      if !blacklist_id
+        new_blacklist = Blacklist.create(
+            website_name: request.env["HTTP_X_BLACKLIST_NAME"],
+            website_url: request.env["HTTP_X_BLACKLIST_URL"]
+          )
+        # 2. Create BlacklistUser
+        BlacklistUser.create(user: User.find(user.id), blacklist: Blacklist.find(new_blacklist.id))
+
+        # Return updated list of blacklists
+        blacklist_user_all = BlacklistUser.where(user: User.find(user.id))
+        @updated_blacklists = blacklist_user_all.map do |blacklist_user|
+          Blacklist.find(blacklist_user.blacklist_id)
+        end
+      end
     end
   end
 end
