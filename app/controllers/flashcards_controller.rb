bb54e708dca89084eef31a45d6e7be99c736bd97
Tom Poulter <tom.poulter.1@outlook.com>
2020-03-12 19:17:42 +0000

    edit details done

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -2,7 +2,8 @@ class FlashcardsController < ApplicationController
 
   def show
     @user = current_user
-    @current_flashcard = Flashcard.find(params[:id])
+    @current_flashcard = FlashcardUser.where(user_id: @user.id).where(completed: false).first.flashcard
     @next_flashcard = Flashcard.find(params[:id].to_i + 1)
   end
+
 end
