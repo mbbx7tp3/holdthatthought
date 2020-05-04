995e34bbe50ac987646282160391a3a1dd05d1e0
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-05-04 16:10:33 +0100

    API tokens#login_return_token now requires correct user password from extension

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -3,9 +3,22 @@ class Api::V1::TokensController < Api::V1::BaseController
 
   def login_return_token
     user_email = request.env["HTTP_X_USER_EMAIL"]
-    # user_password = request.env["HTTP_X_USER_PASSWORD"]
+    user_password = request.env["HTTP_X_USER_PASSWORD"]
+
+    # user = User.find_by(email: user_email)
     user = User.find_by(email: user_email)
-    message = user ? user.authentication_token : "User not found with this email"
-    render json: { message: message }
+
+    if user.valid_password?(user_password)
+      message = {
+        status: 'SUCCESS',
+        authentication_token: user.authentication_token
+      }
+    else
+      message = {
+        status: 'User not found with this email and password',
+      }
+    end
+
+    render json: message
   end
 end
